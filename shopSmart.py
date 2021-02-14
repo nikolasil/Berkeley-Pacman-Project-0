fe043b53f89e8883304c681a409683a1a208429b
Νικόλας Ηλιόπουλος <34923640+nikolasil@users.noreply.github.com>
2021-02-15 00:18:44 +0200

    Add files via upload

---
+++ shopSmart.py
@@ -0,0 +1,57 @@
+# shopSmart.py
+# ------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+Here's the intended output of this script, once you fill it in:
+
+Welcome to shop1 fruit shop
+Welcome to shop2 fruit shop
+For orders:  [('apples', 1.0), ('oranges', 3.0)] best shop is shop1
+For orders:  [('apples', 3.0)] best shop is shop2
+"""
+from __future__ import print_function
+import shop
+
+
+def shopSmart(orderList, fruitShops):
+    """
+        orderList: List of (fruit, numPound) tuples
+        fruitShops: List of FruitShops
+    """
+    "*** YOUR CODE HERE ***"
+    cost = fruitShops[0].getPriceOfOrder(orderList) # Initialize the cost variable with the first shop cost
+    cheapestShop = fruitShops[0] # Initialize the variable cheapestShop with the first shop so if the first shop is the cheapest i will return it
+    
+    iterShops = iter(fruitShops)
+    next(iterShops) # skip the the first shop because we already checked the price of the order for the first shop in the initialization of the variables
+
+    for shop in iterShops:  # loop for the each other shop and in every loop we check if the previus is cheaper or not and update the variables if true
+        temp = shop.getPriceOfOrder(orderList)
+        if(cost > temp):
+            cost = temp
+            cheapestShop = shop
+    return cheapestShop # in the end we will have the cheapest shop so we return it
+
+
+if __name__ == '__main__':
+    "This code runs when you invoke the script from the command line"
+    orders = [('apples', 1.0), ('oranges', 3.0)]
+    dir1 = {'apples': 2.0, 'oranges': 1.0}
+    shop1 = shop.FruitShop('shop1', dir1)
+    dir2 = {'apples': 1.0, 'oranges': 5.0}
+    shop2 = shop.FruitShop('shop2', dir2)
+    shops = [shop1, shop2]
+    print("For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName())
+    orders = [('apples', 3.0)]
+    print("For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName())
